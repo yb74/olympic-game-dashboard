b51dfb529346f47959ced7d607e4e3832c0f8f51
Younes Bouaziz <54204783+yb74@users.noreply.github.com>
2024-08-24 14:42:54 +0200

    loading data from json file and displaying it in template
    creating a function to retrieve data
    
    creating a function to define ring border size based on total medals
    
    displaying data in html template and starting to style the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RingComponent } from './ring/ring.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
